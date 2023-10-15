2c8ff377f94c9c2271ab2f8b197520ad0b76b4a2
isaacfn <isaacferreirati@gmail.com>
2023-10-15 18:44:23 -0300

    Incremento de código

--- src/main/java/br/com/isaacferreira/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/isaacferreira/todolist/filter/FilterTaskAuth.java
@@ -50,8 +50,8 @@ public class FilterTaskAuth extends OncePerRequestFilter {
                     var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
 
                     if(passwordVerify.verified){
-                        request.setAttribute("idUser", user.getId());
-                        filterChain.doFilter(request, response);
+                       request.setAttribute("idUser", user.getId());
+                       filterChain.doFilter(request, response);
                     }else{
                         response.sendError(401);
                     }
--- src/main/java/br/com/isaacferreira/todolist/task/TaskController.java
+++ src/main/java/br/com/isaacferreira/todolist/task/TaskController.java
@@ -1,13 +1,21 @@
 package br.com.isaacferreira.todolist.task;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.isaacferreira.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -18,12 +26,49 @@ public class TaskController {
     private ITaskRepository taskRepository;
     
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
-        System.out.println("Chegou no controller" + request.getAttribute("idUser"));
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
         var idUser = request.getAttribute("idUser");
-        taskModel.setIdUser((UUID)idUser);
+        taskModel.setIdUser((UUID) idUser);
+
+        var currentDate = LocalDateTime.now();
+        if(currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(" A data de início /data de término deve ser maior do que a data atual");        
+        }
+
+        if(taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(" A data de início deve ser menor que a data de término");
+        }
+
         var task = this.taskRepository.save(taskModel);
-       return task;
+        return ResponseEntity.status(HttpStatus.OK).body(task);    
+    }
+
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+        return tasks;
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        if(task == null)
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                     .body("Tarefa não encontrada");
+        
+        var idUser = request.getAttribute("idUser");
+
+        if (!task.getIdUser(). equals("idUser")) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                     .body("Usuário não tem permissão para alterar essa tarefa");
+        }
+
+        Utils.copyNonNullProperties(taskModel, task);
+        var taskUpdate = this.taskRepository.save(task);    
+        return ResponseEntity.ok().body(this.taskRepository.save(taskUpdate));
     }
     
 }
