3c0ada73a1108e44ac3633f0ecd5767af38d1c11
Isaac Ferreira <isaacferreirati@gmail.com>
2023-10-15 12:49:14 -0300

    first commit

---
+++ src/main/java/br/com/isaacferreira/todolist/task/TaskController.java
@@ -0,0 +1,29 @@
+package br.com.isaacferreira.todolist.task;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private ITaskRepository taskRepository;
+    
+    @PostMapping("/")
+    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+        System.out.println("Chegou no controller" + request.getAttribute("idUser"));
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID)idUser);
+        var task = this.taskRepository.save(taskModel);
+       return task;
+    }
+    
+}
---
+++ src/main/java/br/com/isaacferreira/todolist/user/IUserRepository.java
@@ -0,0 +1,12 @@
+package br.com.isaacferreira.todolist.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID>{
+    UserModel findByUsername(String username);
+    
+}
